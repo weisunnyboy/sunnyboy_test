e18c4d9f82719392857cb71d82d2c18bbd34dd70
timo <111939095+weisunnyboy@users.noreply.github.com>
2025-09-13 11:38:42 +0800

    Add files via upload

---
+++ setting_config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@Company :  IMin
+@Time ： 2024/7/26 13:43
+@Auth ： IMin--Timo ewei@imin.com
+@File ：setting_config.py
+@IDE ：PyCharm
+@Motto: IMin--AutoTest
+"""
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_PATH = BASE_DIR + os.sep + "base"
+COMMON_PATH = BASE_DIR + os.sep + "common"
+DATA_PATH = BASE_DIR + os.sep + "data"
+ELEMENTS_PATH = BASE_DIR + os.sep + "elements"
+IMG_PATH = BASE_DIR + os.sep + "img"
+LOGS_PATH = BASE_DIR + os.sep + "logs"
+PAGE_PATH = BASE_DIR + os.sep + "page"
+REPORT_PATH = BASE_DIR + os.sep + "report"
+SCRIPT_PATH = BASE_DIR + os.sep + "script"
+TEMPS_PATH = BASE_DIR + os.sep + "temps"
+TOOLS_PATH = BASE_DIR + os.sep + "tools"
